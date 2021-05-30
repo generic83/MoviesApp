42b285e03806ef1f79c7a688d33d7e90d4ee103e
fadi lutfi <fadi-lutfi@hotmail.com>
2021-05-30 19:58:27 +0200

    Add MovieJsonModel as the target type of JSON value

---
+++ src/MoviesApp/Data/Models/MovieJsonModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MoviesApp.Data.Models
+{
+    public class MovieJsonModel
+    {
+        public string ImdbId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Language { get; set; }
+
+        public string Location { get; set; }
+
+        public string Plot { get; set; }
+
+        public string Poster { get; set; }
+
+        public string[] SoundEffects { get; set; }
+
+        public string[] Stills { get; set; }
+
+        public string ListingType { get; set; }
+
+        public string ImdbRating { get; set; }
+    }
+}
