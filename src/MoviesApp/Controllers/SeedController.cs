0893ad8c9204dd96fe825261a185d447a09f8618
fadi lutfi <fadi-lutfi@hotmail.com>
2021-05-30 20:19:42 +0200

    Add SeedController to be used for deserializing and importing JSON data

---
+++ src/MoviesApp/Controllers/SeedController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApp.Converters;
+using MoviesApp.Data.Models;
+using MoviesApp.SystemIo;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class SeedController : ControllerBase
+    {
+        private readonly IJsonConvert _jsonConvert;
+        private readonly IFile _file;
+
+        public SeedController(IJsonConvert jsonConvert, IFile file)
+        {
+            _jsonConvert = jsonConvert;
+            _file = file;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Import()
+        {
+            using var openStream = _file.OpenRead("Data/Source/movies.json");
+            var data = await _jsonConvert.DeserializeAsync<ICollection<MovieJsonModel>>(openStream, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            
+            //TODO store deserialized data in an in-memory db
+
+            return new JsonResult(new
+            {
+                Movies = data.Count
+            });
+        }
+    }
+}
---
+++ test/MoviesApp.Tests/SeedControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using FizzWare.NBuilder;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+using MoviesApp.Data.Models;
+using MoviesApp.Controllers;
+using MoviesApp.Converters;
+using MoviesApp.SystemIo;
+
+namespace MoviesStore.Tests.Controllers
+{
+    [TestFixture]
+    public class SeedControllerTests
+    {
+        private static Builder _builder;
+        private static IJsonConvert _jsonConvert;
+        private static IFile _file;
+
+        [SetUp]
+        public static void SetUp()
+        {
+            var builderSettings = new BuilderSettings();
+            _builder = new Builder(builderSettings);
+            _jsonConvert = Substitute.For<IJsonConvert>();
+            _file = Substitute.For<IFile>();
+        }
+
+        [Test]
+        public static async Task Import_Seeds_MoviesJsonData()
+        {
+            var sourceFilePath = "Data/Source/movies.json";
+            var sourceStream = new MemoryStream();
+            _file.OpenRead(Arg.Any<string>()).Returns(sourceStream);
+            var deserializedJsonSource = _builder.CreateListOfSize<MovieJsonModel>(10).Build();
+            
+            _jsonConvert.DeserializeAsync<ICollection<MovieJsonModel>>(sourceStream, Arg.Is<JsonSerializerOptions>(x => x.PropertyNameCaseInsensitive == true))
+                .Returns(deserializedJsonSource);
+
+            var controller = new SeedController(_jsonConvert, _file);
+            var result = await controller.Import() as JsonResult;
+
+            _file.Received(1).OpenRead(sourceFilePath);
+            await _jsonConvert.Received(1).DeserializeAsync<ICollection<MovieJsonModel>>(sourceStream, Arg.Any<JsonSerializerOptions>());
+
+            result.Value.Should().NotBeNull();
+        }
+    }
+}
