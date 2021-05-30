cdd69e656e6cd20508cd2374de5c3ad077f43352
fadi lutfi <fadi-lutfi@hotmail.com>
2021-05-30 21:40:55 +0200

    Revert "Install angular material and add angular-material.module.ts"
    This reverts commit 548a84227c07bffbe0abb119fae23aabe68506d8.

--- src/MoviesApp/ClientApp/src/app/app.module.ts
+++ src/MoviesApp/ClientApp/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -20,8 +20,7 @@ import { AngularMaterialModule } from './angular-material.module';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' }
-    ]),
-    AngularMaterialModule
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
